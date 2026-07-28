47753495f3c43182c3656bc9012040b21d1a09ef
rath23 <muneer3@amityonline.com>
2026-07-28 09:58:48 +0530

    implimeted tailwind css to the project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
+import "../../global.css";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-purple-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
